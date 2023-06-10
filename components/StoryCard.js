99a02f9f4a164c08cff3fa4fb7406728d2b6723c
rickydamarsaputra <retrocode.rc@gmail.com>
2023-06-10 22:27:30 +0700

    fix: update

--- components/StoryCard.js
+++ components/StoryCard.js
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function StoryCard({ item }) {
   return (
-    <View className="w-[48.5%] h-[140px] justify-between bg-white mb-3 p-2 shadow-sm shadow-black/50 rounded-lg">
+    <View className="w-[48.5%] h-[145px] justify-between bg-white mb-3 p-2 shadow-sm shadow-black/50 rounded-lg">
       <View>
         <Text className="text-sm text-primary-500 font-medium">{item.author}</Text>
         <Text className="text-sm text-gray-500 mt-2" numberOfLines={3}>{item.content}</Text>
